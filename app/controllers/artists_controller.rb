bc7d35c4763078ff25bb51d24ebc60c15d36fb54
Mariah Morales <mjustine622@gmail.com>
2021-05-30 18:58:13 -0400

    commit tree 9e574ad44469ccda5c5fbf10b6908d8cf6b699f4

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,13 @@
+# rspec spec/features/04_basic_view_spec.rb
 class ArtistsController < ApplicationController
+    get '/artists' do 
+        @artists = Artist.all
+
+        erb :'artists/index'
+    end
+    
+    get '/artists/:slug' do
+        @artist = Artist.find_by_slug(params[:slug])
+        erb :'artists/show'
+    end 
 end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,45 @@
+class SongsController < ApplicationController
+
+    get '/songs' do 
+        @songs = Song.all
+        erb :'songs/index'
+    end
+
+    get '/songs/new' do 
+        @genres = Genre.all 
+        erb :'songs/new'
+    end
+
+    get '/songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        erb :'songs/show'
+    end
+
+    post '/songs' do 
+        song = Song.create(params[:song])
+        song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+        song.genre_ids = params[:genres]
+        song.save
+        # binding.pry
+        redirect "/songs/#{song.slug}"
+
+    end
+
+    get '/songs/:slug/edit' do
+        @genres = Genre.all
+        @song = Song.find_by_slug(params[:slug])
+        erb :'songs/edit'
+    end
+
+    patch '/songs/:slug' do
+        song = Song.find_by_slug(params[:slug])
+        song.update(params[:song])
+        song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+        song.genre_ids = params[:genres]
+        song.save
+
+        redirect "/songs/#{song.slug}"
+    end
+
+
+end
